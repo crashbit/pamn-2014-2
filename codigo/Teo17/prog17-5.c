d6d2de3ed8a462744880509e83be919410fac092
Germán Santos <crashbito@gmail.com>
2014-03-06 22:35:17 -0600

    Inicios de python y programa Gauss-Jordan

--- codigo/Teo17/prog17-5.c
+++ codigo/Teo17/prog17-5.c
@@ -3,6 +3,8 @@ int main(){
 
 	float matriz[3][3]={{3,-0.1,-0.2},{0.1,7,-0.3},{0.3,-0.2,10}};
 	float b[3] = {7.85, -19.3, 71.4};
+	float x[3];
+	float sum;
 	int i=0,j=0,k=0,n=3;
 	float factor;
 
@@ -25,20 +27,20 @@ for(i=0;i<n;i++){
 	printf("\n");
 }
 
+// Aqui inicia Gauss - Jordan, sustitución hacia atrás
+x[n-1] = b[n-1]/matriz[n-1][n-1];
 
+for(i=n-2;i>=0;i--){
+	sum = b[i];
+	for(j = i+1;j<n;j++){
+		sum = sum - (matriz[i][j] * x[j]);
+	}
+	x[i]= sum / matriz[i][i];
+}
 
+for(i = 0; i<n; i++){
 
-
-
-
-
-
-
-
-
-
-
-
-
+	printf("x%d = %f \n",i, x[i]);
+}
 
 }
